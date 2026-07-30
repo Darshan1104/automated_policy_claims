a7b1a2c8b1a77e34e560a9e4743af94cd5041958
Darshan1104 <90019836+Darshan1104@users.noreply.github.com>
2026-07-30 17:56:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from graph import graph
 
 st.set_page_config(
-    page_title="RightCall",
+    page_title="VerdynenAI",
     page_icon="📁",
     layout="wide",  # Changed to wide layout to eliminate scrolling
     initial_sidebar_state="collapsed",
